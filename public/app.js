bb934eaf6571c71f649f83cabe7f4f35b53eae18
Edmond Wu <edma_wu@yahoo.com>
2016-07-25 11:43:03 -0400

    added materialize and finished basic chat functionality

--- public/app.js
+++ public/app.js
@@ -1,9 +1,35 @@
 /**
  * Created by ewu on 7/22/2016.
  */
-var socket = io();
+
+var app = angular.module('chat', []);
+
+app.controller('chatCtrl', function($scope, $timeout) {
+    var socket = io(); //use socket
+
+    //App fields
+    $scope.chatMessage = "";
+    $scope.messages = [];
+
+    $scope.send = function() {
+        if ($scope.chatMessage !== "" && $scope.chatMessage.length <= 500) {
+            socket.emit('chat message', $scope.chatMessage);
+            $scope.chatMessage = "";
+        }
+    };
+    
+    socket.on('message', function(msg) {
+        $scope.$apply(function() {
+            $scope.messages.push(msg);
+        });
+    });
+});
+
+
+/*
 $('form').submit(function(){
     socket.emit('chat message', $('#m').val());
     $('#m').val('');
     return false;
 });
+*/
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//serve public directory
+app.use(express.static(__dirname + '/public'));
+
+//send index page
 app.get('/', function(req, res) {
+	//sendFile for single page, redirect for multi-page (angular routing)
 	res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
 	socket.on('chat message', function(msg) {
-		console.log('message: ' + msg);
+		io.emit("message", msg);
 	});
 	socket.on('disconnect', function() {
 		console.log('user disconnected');
