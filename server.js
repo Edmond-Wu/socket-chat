283ff64a36cf13fbb4f23042f7b5b20eb460d372
Edmond Wu <edma_wu@yahoo.com>
2017-07-21 17:45:57 -0400

    turned port into a variable

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io').listen(http);
+var port = 3000;
 
 //serve public directory
 app.use(express.static(__dirname + '/public'));
@@ -50,6 +51,6 @@ io.on('connection', function(socket) {
 	});
 });
 
-http.listen(3000, function() {
-	console.log('listening on port 3000');
+http.listen(port, function() {
+	console.log('listening on port %d', port);
 });
